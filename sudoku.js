21628be0ea867c8f6c22569f42d890820c340f1a
Your Name <localadmin@compozed-tss-10.mobile.asu.edu>
2016-07-06 17:00:56 -0700

    day 2

--- sudoku.js
+++ sudoku.js
@@ -2,6 +2,13 @@ function Sudoku(allRows, name) {
   this.allRows = allRows;
 }
 
+Sudoku.prototype.checkEverything = function() {
+  if (this.checkAllRows() && this.checkAllCols() && this.checkAllBoxes()) {
+    return true;
+  }
+  return false;
+}
+
 Sudoku.prototype.displayAll = function() {
   for (var y = 0; y < 9; y++) {
     for (var x = 0; x < 9; x++) {
@@ -25,7 +32,7 @@ Sudoku.prototype.displayGrid = function() {
 function checkArrayWorks(array) {
   var hashMap = {};
   for (var val of array) {
-    if (hashMap[val]) { // if something is already here (we found a duplicate)
+    if (hashMap[val] || val === 0) { // if something is already here (we found a duplicate)
       return false;
     } else { // if this value hasnt been added yet
       hashMap[val] = 1;
@@ -35,20 +42,239 @@ function checkArrayWorks(array) {
 }
 
 Sudoku.prototype.getCols = function() {
-  
+  var allCols = [];
+  for (var col = 0; col < 9; col++) {
+    allCols[col] = [];
+    for (var row = 0; row < 9; row++) {
+      allCols[col].push(this.allRows[row][col])
+    }
+  }
+  return allCols;
 }
 
-Sudoku.prototype.checkAllRows = function(){
-  for(var row of this.allRows){
-    if (checkArrayWorks(row) == false){
+Sudoku.prototype.checkAllRows = function() {
+  for (var row of this.allRows) {
+    if (checkArrayWorks(row) == false) {
       return false
     }
   }
   return true;
 }
 
+Sudoku.prototype.checkAllCols = function() {
+  for (var col of this.getCols()) { // one element of array of cols
+    if (checkArrayWorks(col) == false) {
+      return false
+    }
+  }
+  return true;
+}
+
+Sudoku.prototype.checkAllBoxes = function() {
+  for (var box of this.getBoxes()) { // one element of array of cols
+    if (checkArrayWorks(box) == false) {
+      return false
+    }
+  }
+  return true;
+}
+
+Sudoku.prototype.countNumBlanks = function() {
+  var tracker = 0;
+  for (var row of this.allRows) { // for each row
+    for (var element of row) { // for each col within a row
+      if (element === 0) {
+        tracker += 1
+      }
+    }
+  }
+  return tracker;
+}
+
+
+
+Sudoku.prototype.getBoxes = function() {
+  var boxRow1 = this.allRows.filter(function(val, index) {
+    return index < 3;
+  });
+  var boxRow2 = this.allRows.filter(function(val, index) {
+    return index > 2 && index < 6;
+  });
+  var boxRow3 = this.allRows.filter(function(val, index) {
+    return index > 5;
+  });
+
+  var boxRow1Col1 = [];
+  boxRow1.forEach(function(curRow, index, arr) {
+    boxRow1Col1.push(curRow[0]);
+    boxRow1Col1.push(curRow[1]);
+    boxRow1Col1.push(curRow[2]);
+  });
+
+  var boxRow1Col2 = [];
+  boxRow1.forEach(function(curRow, index, arr) {
+    boxRow1Col2.push(curRow[3]);
+    boxRow1Col2.push(curRow[4]);
+    boxRow1Col2.push(curRow[5]);
+  });
+
+  var boxRow1Col3 = [];
+  boxRow1.forEach(function(curRow, index, arr) {
+    boxRow1Col3.push(curRow[6]);
+    boxRow1Col3.push(curRow[7]);
+    boxRow1Col3.push(curRow[8]);
+  });
+
+  var boxRow2Col1 = [];
+  boxRow2.forEach(function(curRow, index, arr) {
+    boxRow2Col1.push(curRow[0]);
+    boxRow2Col1.push(curRow[1]);
+    boxRow2Col1.push(curRow[2]);
+  });
+
+  var boxRow2Col2 = [];
+  boxRow2.forEach(function(curRow, index, arr) {
+    boxRow2Col2.push(curRow[3]);
+    boxRow2Col2.push(curRow[4]);
+    boxRow2Col2.push(curRow[5]);
+  });
+
+  var boxRow2Col3 = [];
+  boxRow2.forEach(function(curRow, index, arr) {
+    boxRow2Col3.push(curRow[6]);
+    boxRow2Col3.push(curRow[7]);
+    boxRow2Col3.push(curRow[8]);
+  });
+
+  var boxRow3Col1 = [];
+  boxRow3.forEach(function(curRow, index, arr) {
+    boxRow3Col1.push(curRow[0]);
+    boxRow3Col1.push(curRow[1]);
+    boxRow3Col1.push(curRow[2]);
+  });
+
+  var boxRow3Col2 = [];
+  boxRow3.forEach(function(curRow, index, arr) {
+    boxRow3Col2.push(curRow[3]);
+    boxRow3Col2.push(curRow[4]);
+    boxRow3Col2.push(curRow[5]);
+  });
+
+  var boxRow3Col3 = [];
+  boxRow3.forEach(function(curRow, index, arr) {
+    boxRow3Col3.push(curRow[6]);
+    boxRow3Col3.push(curRow[7]);
+    boxRow3Col3.push(curRow[8]);
+  });
+
+  var allBoxes = [];
+  allBoxes.push(boxRow1Col1, boxRow1Col2, boxRow1Col3,
+    boxRow2Col1, boxRow2Col2, boxRow2Col3,
+    boxRow3Col1, boxRow3Col2, boxRow3Col3);
+
+  return allBoxes;
+}
+
+Sudoku.prototype.putVals = function(array) {
+  for (var row of this.allRows) { // for each row
+    for (var index in row) { // for each col within a row
+      if (row[index] === 0) {
+        row[index] = array.shift(); //array.shift() removes and returns the first element of the array
+      }
+    }
+  }
+}
+
+
+
+
+Sudoku.prototype.solve = function() {
+  console.log(this.displayGrid());
+  // figure out how many numbers we need
+  var numInts = this.countNumBlanks();
+
+  // set up the testSolution array
+  var testSolutionArr = [];
+  for (var i = 0; i < numInts; i++) {
+    testSolutionArr.push(1);
+  }
+
+  //copy arr1 into arr2
+  function copyArray(arr1, solvingSudoku) {
+    solvingSudoku.allRows = [];
+    for (var y = 0; y < arr1.length; y++) {
+      solvingSudoku.allRows[y] = [];
+      for (var x = 0; x < arr1[y].length; x++) {
+        var val = arr1[y][x];
+        //console.log('val is '+ val);
+        solvingSudoku.allRows[y].push(val);
+      }
+    }
+    console.log('here is arr2: ');
+    console.log(solvingSudoku.displayGrid());
+  }
+
+
+  // we create a copy of the main sudoku
+  var solvingSudoku = new Sudoku([]);
+  copyArray(this.allRows, solvingSudoku);
+  console.log('here is solvingSudoku (should have 0s): \n' + solvingSudoku.displayGrid());
+  console.log('here is mainSudoku (should have 0s): \n' + this.displayGrid());
+
+  // we go through a loop until we solve the sudoku
+  var solved = false;
+  while (solved === false) {
+    console.log('trying: ' + testSolutionArr + '\n');
+    solvingSudoku.putVals(testSolutionArr); // input our test values
+    if (solvingSudoku.checkEverything() === true) {
+      solved = true;
+    } else {
+      copyArray(this.allRows, solvingSudoku.allRows); // set solvingSudoku back to normal
+
+      console.log('here is solvingSudoku (should have 0s): \n' + solvingSudoku.displayGrid());
+
+      if (alreadyTriedMax(testSolutionArr)) { // if we've already tried every possible solution...
+        console.log('*****ERROR COULD NOT FIND SOLUTION***** WAS TRYING ' + testSolutionArr.length);
+        return 'error';
+      }
+      testSolutionArr = nextTestSolutionArr(testSolutionArr); // get the next solution
+    }
+  }
+
+  //when solvingSudoku is is finally correct...
+  console.log(solvingSudoku.displayGrid());
+  return solvingSudoku;
+}
+
+function alreadyTriedMax(array) { // check if every value in the array is 9
+  console.log('the array is :' + array);
+  for (var num of array) {
+    if (num != 9) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function nextTestSolutionArr(array) {
+  var solutionStr = '';
+  for (var num of array) {
+    solutionStr += num;
+  }
+  var solutionInt = parseInt(solutionStr);
+  solutionInt++;
+  solutionStr = solutionInt.toString();
+  var solutionArr = [];
+  for (var char of solutionStr) {
+    solutionArr.push(parseInt(char));
+  }
+  return solutionArr;
+}
+
 
 module.exports = {
   Sudoku: Sudoku,
-  checkArrayWorks: checkArrayWorks
+  checkArrayWorks: checkArrayWorks,
+  nextTestSolutionArr: nextTestSolutionArr,
+  alreadyTriedMax: alreadyTriedMax
 };
--- test/unit_test/sudoku_test.js
+++ test/unit_test/sudoku_test.js
@@ -1,6 +1,10 @@
+
+
 var expect = require('chai').expect;
 var Sudoku = require('../../sudoku').Sudoku;
 var checkArrayWorks = require('../../sudoku').checkArrayWorks;
+var nextTestSolutionArr = require('../../sudoku').nextTestSolutionArr;
+var alreadyTriedMax = require('../../sudoku').alreadyTriedMax;
 var emptyRow = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 var allRowsEmpty = [
@@ -13,43 +17,105 @@ var allRowsEmpty = [
   emptyRow,
   emptyRow,
   emptyRow
-  ];
+];
 
-  var correctArray = [
-                        [1, 3, 5, 8, 4, 6, 2, 9, 7],
-                        [6, 7, 4, 3, 9, 2, 5, 8, 1],
-                        [8, 9, 2, 1, 5, 7, 3, 4, 6],
-                        [2, 8, 9, 4, 1, 3, 6, 7, 5],
-                        [3, 4, 7, 5, 6, 8, 1, 2, 9],
-                        [5, 6, 1, 7, 2, 9, 4, 3, 8],
-                        [7, 1, 8, 6, 3, 4, 9, 5, 2],
-                        [9, 5, 3, 2, 8, 1, 7, 6, 4],
-                        [4, 2, 6, 9, 7, 5, 8, 1, 3]
-  ];
+var correctArray = [
+  [1, 3, 5, 8, 4, 6, 2, 9, 7],
+  [6, 7, 4, 3, 9, 2, 5, 8, 1],
+  [8, 9, 2, 1, 5, 7, 3, 4, 6],
+  [2, 8, 9, 4, 1, 3, 6, 7, 5],
+  [3, 4, 7, 5, 6, 8, 1, 2, 9],
+  [5, 6, 1, 7, 2, 9, 4, 3, 8],
+  [7, 1, 8, 6, 3, 4, 9, 5, 2],
+  [9, 5, 3, 2, 8, 1, 7, 6, 4],
+  [4, 2, 6, 9, 7, 5, 8, 1, 3]
+];
 
-  var incorrectArray = [
-                        [1, 3, 5, 8, 4, 6, 2, 9, 7],
-                        [6, 7, 4, 3, 9, 2, 5, 8, 1],
-                        [8, 9, 2, 1, 5, 7, 3, 4, 6],
-                        [2, 8, 9, 1, 1, 3, 6, 7, 5],
-                        [3, 4, 7, 5, 6, 8, 1, 2, 9],
-                        [5, 6, 1, 7, 2, 9, 4, 3, 8],
-                        [7, 1, 8, 6, 3, 4, 9, 5, 2],
-                        [9, 5, 3, 2, 8, 1, 7, 6, 4],
-                        [4, 2, 6, 9, 7, 5, 8, 1, 3]
-  ];
+var incorrectArray = [
+  [1, 3, 5, 8, 4, 6, 2, 9, 7],
+  [6, 7, 4, 3, 9, 2, 5, 8, 1],
+  [8, 9, 2, 1, 5, 7, 3, 4, 6],
+  [2, 8, 9, 1, 1, 3, 6, 7, 5],
+  [3, 4, 7, 5, 6, 8, 1, 2, 9],
+  [5, 6, 1, 7, 2, 9, 4, 3, 8],
+  [7, 1, 8, 6, 3, 4, 9, 5, 2],
+  [9, 5, 3, 2, 8, 1, 7, 6, 4],
+  [4, 2, 6, 9, 7, 5, 8, 1, 3]
+];
+
+var unsolvedArray = [
+  [1, 3, 5, 8, 4, 6, 2, 9, 7],
+  [6, 7, 4, 3, 0, 2, 5, 8, 1],
+  [8, 9, 2, 1, 5, 7, 3, 4, 6],
+  [0, 8, 9, 4, 1, 3, 6, 7, 0],
+  [3, 4, 7, 5, 6, 8, 1, 2, 0],
+  [5, 6, 1, 0, 2, 9, 4, 3, 8],
+  [7, 1, 8, 6, 3, 4, 9, 5, 2],
+  [9, 5, 3, 2, 8, 1, 7, 6, 4],
+  [4, 2, 6, 9, 7, 5, 8, 1, 3]
+];
+
+var unsolvedInputArray = [
+  [1, 3, 5, 8, 4, 6, 2, 9, 7],
+  [6, 7, 4, 3, 0, 2, 5, 8, 1],
+  [8, 9, 2, 1, 5, 7, 3, 4, 6],
+  [0, 8, 9, 4, 1, 3, 6, 7, 0],
+  [3, 4, 7, 5, 6, 8, 1, 2, 0],
+  [5, 6, 1, 0, 2, 9, 4, 3, 8],
+  [7, 1, 8, 6, 3, 4, 9, 5, 2],
+  [9, 5, 3, 2, 8, 1, 7, 6, 4],
+  [4, 2, 6, 9, 7, 5, 8, 1, 3]
+];
+
+var unsolvedBrokenArray = [
+  [3, 3, 5, 8, 4, 6, 2, 9, 7],
+  [6, 7, 4, 3, 0, 2, 5, 8, 1],
+  [8, 9, 2, 1, 5, 7, 3, 4, 6],
+  [0, 8, 9, 4, 1, 3, 6, 7, 0],
+  [3, 4, 7, 5, 6, 8, 1, 2, 0],
+  [5, 6, 1, 0, 2, 9, 4, 3, 8],
+  [7, 1, 8, 6, 3, 4, 9, 5, 2],
+  [9, 5, 3, 2, 8, 1, 7, 6, 4],
+  [4, 2, 6, 9, 7, 5, 8, 1, 3]
+];
+
+var unsolvedHARDArray = [
+  [8, 0, 6, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 6, 0, 0, 0, 0, 0],
+  [0, 0, 0, 9, 0, 3, 2, 0, 0],
+  [9, 3, 0, 2, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 2, 6],
+  [0, 0, 0, 3, 4, 0, 5, 0, 0],
+  [1, 0, 0, 0, 8, 0, 0, 0, 0],
+  [3, 0, 0, 0, 0, 7, 0, 0, 4],
+  [0, 0, 4, 0, 0, 0, 0, 0, 0]
+];
+
+var correctHARDArray = [
+  [8, 1, 6, 7, 2, 4, 9, 3, 5],
+  [2, 9, 3, 6, 5, 8, 7, 4, 1],
+  [4, 7, 5, 9, 1, 3, 2, 6, 8],
+  [9, 3, 8, 2, 6, 5, 4, 1, 7],
+  [5, 4, 1, 8, 7, 9, 3, 2, 6],
+  [6, 2, 7, 3, 4, 1, 5, 8, 9],
+  [1, 5, 9, 4, 8, 2, 6, 7, 3],
+  [3, 6, 2, 1, 9, 7, 8, 5, 4],
+  [7, 8, 4, 5, 3, 6, 1, 9, 2]
+];
 
 var emptyTable = new Sudoku(allRowsEmpty); // creates the object
 var correctTable = new Sudoku(correctArray); // creates the object
 var incorrectTable = new Sudoku(incorrectArray); // creates the object
+var unsolvedTable = new Sudoku(unsolvedArray);
+var unsolvedBrokenTable = new Sudoku(unsolvedBrokenArray);
+var unsolvedHARDTable = new Sudoku(unsolvedHARDArray);
 
-
-describe('Soduko', function(){
-  describe('creating sudoku tables', function () {
-    it('table should have 9 rows', function(){
+describe('Soduko', function() {
+  describe('creating sudoku tables', function() {
+    it('table should have 9 rows', function() {
       expect(emptyTable.allRows.length).to.equal(9);
     });
-    it('each row should have 9 values', function(){
+    it('each row should have 9 values', function() {
       expect(emptyTable.allRows[0].length).to.equal(9)
       expect(emptyTable.allRows[1].length).to.equal(9);
       expect(emptyTable.allRows[2].length).to.equal(9);
@@ -65,54 +131,140 @@ describe('Soduko', function(){
       expect(emptyTable.allRows[8][8]).to.equal(0);
       expect(emptyTable.allRows[3][5]).to.equal(0);
     });
-    it('each value in a row is between 0 and 9', function(){
-      for(var y = 0; y < 9; y++){
-        for(var x = 0; x < 9; x++){
+    it('each value in a row is between 0 and 9', function() {
+      for (var y = 0; y < 9; y++) {
+        for (var x = 0; x < 9; x++) {
           expect(emptyTable.allRows[y][x]).to.equal(0);
         }
       }
     })
   });
-  describe('displaying sudoku table in the form of a grid', function(){
-    it('should display as a grid', function(){
+  describe('displaying sudoku table in the form of a grid', function() {
+    it('should display as a grid', function() {
       expect(emptyTable.displayGrid()).
-              to.deep.equal('0 0 0 0 0 0 0 0 0 \n' +
-                            '0 0 0 0 0 0 0 0 0 \n' +
-                            '0 0 0 0 0 0 0 0 0 \n' +
-                            '0 0 0 0 0 0 0 0 0 \n' +
-                            '0 0 0 0 0 0 0 0 0 \n' +
-                            '0 0 0 0 0 0 0 0 0 \n' +
-                            '0 0 0 0 0 0 0 0 0 \n' +
-                            '0 0 0 0 0 0 0 0 0 \n' +
-                            '0 0 0 0 0 0 0 0 0 \n');
+      to.deep.equal('0 0 0 0 0 0 0 0 0 \n' +
+        '0 0 0 0 0 0 0 0 0 \n' +
+        '0 0 0 0 0 0 0 0 0 \n' +
+        '0 0 0 0 0 0 0 0 0 \n' +
+        '0 0 0 0 0 0 0 0 0 \n' +
+        '0 0 0 0 0 0 0 0 0 \n' +
+        '0 0 0 0 0 0 0 0 0 \n' +
+        '0 0 0 0 0 0 0 0 0 \n' +
+        '0 0 0 0 0 0 0 0 0 \n');
     });
     //console.log(emptyTable.displayGrid());
   });
-  describe('getCols', function(){
-    it('returns an array of arrays of each collumn in a Sudoku table', function(){
-        var colsArr = correctTable.getCols();
-        expect(colsArr).to.equal()
-    });
-  });
-  describe('checking if table is correct', function(){
-    describe('checking rows', function(){
-      it('returns true if a specific row is correct', function(){
-        var newRow = [1,2,3,4,5,6,7,8,9];
-        expect(checkArrayWorks(newRow)).to.equal(true);
-      });
-      it('returns false if a specific row is incorrect', function(){
-        var newRow = [1,2,3,4,5,7,7,8,9];
-        expect(checkArrayWorks(newRow)).to.equal(false);
-      });
-      it('returns true if an entire board is correct (only checking rows)', function(){
-        var result = correctTable.checkAllRows();
-        expect(result).to.equal(true);
-      });
-      it('returns false if an entire board is incorrect (only checking rows)', function(){
-        var result = incorrectTable.checkAllRows();
-        expect(result).to.equal(false);
-      });
+  describe('getCols', function() {
+    it('returns an array of arrays of each collumn in a Sudoku table', function() {
+      var colsArr = correctTable.getCols();
+      expect(colsArr).to.deep.equal([
+        [1, 6, 8, 2, 3, 5, 7, 9, 4],
+        [3, 7, 9, 8, 4, 6, 1, 5, 2],
+        [5, 4, 2, 9, 7, 1, 8, 3, 6],
+        [8, 3, 1, 4, 5, 7, 6, 2, 9],
+        [4, 9, 5, 1, 6, 2, 3, 8, 7],
+        [6, 2, 7, 3, 8, 9, 4, 1, 5],
+        [2, 5, 3, 6, 1, 4, 9, 7, 8],
+        [9, 8, 4, 7, 2, 3, 5, 6, 1],
+        [7, 1, 6, 5, 9, 8, 2, 4, 3]
+      ]);
 
     });
   });
+  describe('checking if table is correct', function() {
+    describe('checking rows', function() {
+      it('returns true if a specific row is correct', function() {
+        var newRow = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        expect(checkArrayWorks(newRow)).to.equal(true);
+      });
+      it('returns false if a specific row is incorrect', function() {
+        var newRow = [1, 2, 3, 4, 5, 7, 7, 8, 9];
+        expect(checkArrayWorks(newRow)).to.equal(false);
+      });
+      it('returns true if an entire board is correct (only checking rows)', function() {
+        var result = correctTable.checkAllRows();
+        expect(result).to.equal(true);
+      });
+      it('returns false if an entire board is incorrect (only checking rows)', function() {
+        var result = incorrectTable.checkAllRows();
+        expect(result).to.equal(false);
+      });
+      describe('checking cols', function() {
+        it('returns true if an entire board is correct (only checking cols)', function() {
+          var result = correctTable.checkAllCols();
+          expect(result).to.equal(true);
+        });
+        it('returns false if an entire board is incorrect (only checking cols)', function() {
+          var result = incorrectTable.checkAllCols();
+          expect(result).to.equal(false);
+        });
+      })
+      describe('checking boxes', function() {
+        it('returns true if an entire board is correct (only checking boxes)', function() {
+          var result = correctTable.checkAllBoxes();
+          expect(result).to.equal(true);
+        });
+        it('returns false if an entire board is incorrect (only checking boxes)', function() {
+          var result = incorrectTable.checkAllBoxes();
+          expect(result).to.equal(false);
+        });
+      });
+      describe('checks everything', function() {
+        it('returns true if everything is correct', function(){
+          var result = correctTable.checkEverything();
+          expect(result).to.equal(true);
+        });
+        it('returns false if anything is incorrect', function(){
+          var result = incorrectTable.checkEverything();
+          expect(result).to.equal(false);
+        });
+      });
+    });
+  });
+  describe('nextTestSolutionArr', function(){
+    it('increments by 1', function(){
+      var arr = [1, 1, 1];
+      var res = nextTestSolutionArr(arr);
+      expect(res).to.deep.equal([1, 1, 2]);
+    });
+    it('increments by 1 and carries over', function(){
+      var arr = [2, 9, 9];
+      var res = nextTestSolutionArr(arr);
+      expect(res).to.deep.equal([3, 0, 0]);
+    });
+    it('alreadyTriedMax returns true if all values are 9', function(){
+      var arr = [9, 9, 9];
+      var res = alreadyTriedMax(arr);
+      expect(res).to.equal(true);
+    });
+    it('alreadyTriedMax returns false if all values are not 9', function(){
+      var arr = [9, 8, 9];
+      var res = alreadyTriedMax(arr);
+      expect(res).to.equal(false);
+    });
+  });
+  describe('solving sudoku table', function() {
+    it('counts the number of blank numbers/ placeholders in the board', function(){
+      var result = unsolvedTable.countNumBlanks();
+      expect(result).to.equal(5)
+    });
+    it('can replace blanks with numbers from an array', function(){
+      var testSudoku = new Sudoku(unsolvedInputArray);
+      testSudoku.putVals([9, 2, 5, 9, 7]);
+      var result = testSudoku.allRows;
+      expect(result).to.deep.equal(correctArray);
+    })
+    it('returns the solved table', function(){
+      var result = unsolvedTable.solve().allRows;
+      expect(result).to.deep.equal(correctArray);
+    });
+    xit('returns an error if it cant be solved', function(){
+      var result = unsolvedBrokenTable.solve();
+      expect(result).to.equal('error');
+    });
+    xit('returns the solved HARD puzzle', function(){
+      var result = unsolvedHARDTable.solve();
+      expect(result).to.equal(correctHARDArray);
+    });
+  })
 });
